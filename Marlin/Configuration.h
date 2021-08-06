e5e37b54776f1efef427b1a16f7a538085519879
Bastian <bastian@bastian.com>
2021-08-06 20:45:34 +0200

    Endstoppulldowns activated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1089,7 +1089,7 @@
 #endif
 
 // Enable pulldown for all endstops to prevent a floating state
-//#define ENDSTOPPULLDOWNS
+#define ENDSTOPPULLDOWNS
 #if DISABLED(ENDSTOPPULLDOWNS)
   // Disable ENDSTOPPULLDOWNS to set pulldowns individually
   //#define ENDSTOPPULLDOWN_XMIN
