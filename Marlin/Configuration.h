b0ea81cf08b342826a0012bc4281db05da2ae692
Bastian <bastian@bastian.com>
2021-08-06 19:50:54 +0200

    Corrected Z steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -806,7 +806,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 56.14, 418.59 }       //https://e3d-online.dozuki.com/Guide/Titan+Aero+Marlin+Configuration/39
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 20469.33, 418.59 }       //https://e3d-online.dozuki.com/Guide/Titan+Aero+Marlin+Configuration/39
 
 /**
  * Default Max Feed Rate (mm/s)
