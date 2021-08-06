68a8ea04a110420e6c305deaa6d00db1f1a794bd
Bastian <bastian@bastian.com>
2021-08-06 20:13:23 +0200

    changed Y-Stop/Z-Stop

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -61,8 +61,8 @@
 // TMC StallGuard DIAG pins
 //
 #define X_DIAG_PIN                         P1_29  // X-STOP
-#define Y_DIAG_PIN                         P1_27  // Y-STOP
-#define Z_DIAG_PIN                         P1_28  // Z-STOP
+#define Y_DIAG_PIN                         P1_28  // Y-STOP
+#define Z_DIAG_PIN                         P1_27  // Z-STOP
 #define E0_DIAG_PIN                        P1_26  // E0DET
 #define E1_DIAG_PIN                        P1_25  // E1DET
 
