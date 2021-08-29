5214dcc6e21157b1f1d57c8d51e402e70efec104
Bastian <bastian@bastian.com>
2021-08-29 20:49:44 +0200

    PID Values Hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -517,9 +517,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  37.52
+    #define DEFAULT_Ki   3.20
+    #define DEFAULT_Kd 109.79
   #endif
 #endif // PIDTEMP
 
