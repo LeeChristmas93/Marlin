72621350fe04dd1ee6e1a0793572b8200a3760d1
Bastian <bastian@bastian.com>
2022-07-13 16:07:31 +0200

    Extruder current to 1500mA

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2858,7 +2858,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      2000
+    #define E0_CURRENT      1500
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
